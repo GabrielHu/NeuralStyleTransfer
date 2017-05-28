8df720bd09244caca1c607f5885b25e195d9130e
RenjiuHu <505218636@qq.com>
2017-05-28 14:43:49 +0800

    A probably runable program

--- image/Multi-style_transfer.py
+++ image/Multi-style_transfer.py
@@ -1,59 +1,21 @@
 '''
 This is the keras version of http://arxiv.org/abs/1610.07629, a paper about multi-style transfer.
-Comparing to J.Johnson et al., this paper replaces transpose convolution layer with nearest-neighbor 
+Comparing to J.Johnson et al., this paper replaces transpose convolution layer with nearest-neighbor
 interpolation.
 '''
-
 import numpy as np
+import utils
 from keras.applications import vgg16
-from scipy.misc import imsave
 from keras.layers.convolutional import Conv2D, UpSampling2D
-from keras.layers.normalization import BatchNormalization
-from keras.layers import Activation, add, Input
+from keras.layers import add, Input
 from keras.models import Sequential, Model
-from keras.engine.topology import Layer
-from keras.preprocessing.image import img_to_array, load_img
 from keras import backend as K
-
-
-def preprocess_img(img_path):
-    '''Change image tensor to fit VGG-network.
-
-    Args: 
-        img_path: a string. location of unprocessed image.
-
-    Return:
-        a 4-D tensor. Input of VGG-network.
-    '''
-    img = load_img(img_path, target_size=(256, 256))
-    img = img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    return vgg16.preprocess_input(img)
-
-
-def deprocess_img(img):
-    '''Change VGG tensor to image matrix.
-
-    Args: 
-        img: a 4-D tensor. a processed 4-D tensor.
-
-    Return:
-        a 3-D tensor. The tensor of generated image.
-    '''
-    img = img.reshape((256, 256, 3))
-    img[:, :, 0] += 103.939
-    img[:, :, 1] += 116.779
-    img[:, :, 2] += 123.68
-    # 'BGR'->'RGB'
-    img = img[:, :, ::-1]
-    img = np.clip(img, 0, 255).astype('uint8')
-    return img
 
 
 def gram_matrix(img_matrix):
     '''Calculate gram matrix.
 
-    Args: 
+    Args:
         img_matrix: a 4-D tensor. a processed 4-D tensor.
 
     Return:
@@ -68,7 +30,7 @@ def gram_matrix(img_matrix):
 def style_loss(style, combination):
     '''Calculate style loss.
 
-    Args: 
+    Args:
         style: a 4-D tensor. a processed 4-D tensor of style picture.
         combination: a 4-D tensor. a processed 4-D tensor of combination picture.
 
@@ -87,7 +49,7 @@ def style_loss(style, combination):
 def content_loss(base, combination):
     '''Calculate style loss. Designed to transfer style. Designed to maintain the "content" of the base image in the generated image
 
-    Args: 
+    Args:
         base: a 4-D tensor. a processed 4-D tensor of content picture.
         combination: a 4-D tensor. a processed 4-D tensor of combination picture.
 
@@ -100,7 +62,7 @@ def content_loss(base, combination):
 def total_variation_loss(x):
     '''Calculate style loss. Designed to keep the generated image locally coherent
 
-    Args: 
+    Args:
         x: a 4-D tensor. a processed 4-D tensor of combination picture.
 
     Return:
@@ -114,42 +76,10 @@ def total_variation_loss(x):
     return K.sum(K.pow(a + b, 1.25))
 
 
-def loss(combination_image):
-    '''The loss function to minimize
-
-    Args:
-        input_tensor: a 4-D tensor. concatenation of images.
-
-    Return:
-        a scalar. 
-    '''
-
-    outputs_dict = dict([(layer.name, layer.output) for layer in pre_model.layers])
-    loss = K.variable(0.)
-    layer_features = outputs_dict['block4_conv2']
-
-    base_image_features = layer_features[0, :, :, :]
-    combination_features = layer_features[2, :, :, :]
-    loss += content_loss(base_image_features,
-                         combination_features)
-
-    feature_layers = ['block1_conv1', 'block2_conv1',
-                      'block3_conv1', 'block4_conv1',
-                      'block5_conv1']
-    for layer_name in feature_layers:
-        layer_features = outputs_dict[layer_name]
-        style_reference_features = layer_features[1, :, :, :]
-        combination_features = layer_features[2, :, :, :]
-        sl = style_loss(style_reference_features, combination_features)
-        loss += (0.1 / len(feature_layers)) * sl
-    loss += 0.05 * total_variation_loss(combination_image)
-    return loss
-
-
 def _residual_block(input_shape):
     '''a residual block.
 
-    Args: 
+    Args:
         input_shape: a list. indication of tensor's size.
 
     Return:
@@ -158,7 +88,7 @@ def _residual_block(input_shape):
     input = Input(shape=input_shape)
     output = Conv2D(filters=128, kernel_size=(3, 3),
                     padding='same', activation='relu')(input)
-    output = Conv2D(filters=128, kernel_size=(3, 3), padding='same')(input)
+    output = Conv2D(filters=128, kernel_size=(3, 3), padding='same')(output)
     output = add([input, output])
     return Model(inputs=input, outputs=output)
 
@@ -166,7 +96,7 @@ def _residual_block(input_shape):
 def _upsampling_block(input_shape, filters):
     '''a transpose convolution block.
 
-    Args: 
+    Args:
         input_shape: a list. indication of tensor's size.
         filters: number of filters.
         kernel_size: the size of kernals.
@@ -176,40 +106,101 @@ def _upsampling_block(input_shape, filters):
         a 4-D tensor. The output after operation.
     '''
     input = Input(shape=input_shape)
-    output = UpSampling2D(input)
+    output = UpSampling2D(size=(2, 2))(input)
     output = Conv2D(filters=filters, kernel_size=(
-        3, 3), padding='same', activation='relu')
+        3, 3), padding='same', activation='relu')(output)
     return Model(inputs=input, outputs=output)
 
 
+def loss(content_img, combination_img):
+    '''The loss function to minimize
 
-base_img = K.concatenate()
-combination_img = K.variable(preprocess_image(np.random.uniform(0,255,K.shape(base_img)))
+    Args:
+        input_tensor: a 4-D tensor. concatenation of images.
+
+    Return:
+        a scalar.
+    '''
+    style_path = r'C:\Users\Renjiu Hu\Pictures\training data\style\\'
+    files = utils.list_files(style_path)
+    style_label = [utils.get_img(
+        style_path + img).astype('float64') for img in files]
+    style_list = [K.variable(vgg16.preprocess_input(np.expand_dims(
+        utils.get_img(style_path + img).astype('float64'), axis=0))) for img in files]
+    style_img = K.concatenate(style_list, axis=0)
+
+    content_model = vgg16.VGG16(input_tensor=content_img,
+                                weights='imagenet', include_top=False)
+    outputs_dict_content = dict([(layer.name, layer.output)
+                                 for layer in content_model.layers])
+    style_model = vgg16.VGG16(input_tensor=style_img,
+                              weights='imagenet', include_top=False)
+    outputs_dict_style = dict([(layer.name, layer.output)
+                               for layer in style_model.layers])
+    combination_model = vgg16.VGG16(input_tensor=combination_img,
+                                    weights='imagenet', include_top=False)
+    outputs_dict_combination = dict([(layer.name, layer.output)
+                                     for layer in combination_model.layers])
+
+    loss = []
+    layer_features_content = outputs_dict_content['block4_conv2']
+    layer_features_combination = outputs_dict_combination['block4_conv2']
+    for i in range(np.shape(K.eval(combination_img))[0]):
+        loss.append(content_loss(layer_features_content[i, :, :, :],
+                                 layer_features_combination[i, :, :, :]))
+
+    feature_layers = ['block1_conv1', 'block2_conv1',
+                      'block3_conv1', 'block4_conv1',
+                      'block5_conv1']
+    for layer_name in feature_layers:
+        layer_features_style = outputs_dict_style[layer_name]
+        layer_features_combination = outputs_dict_combination[layer_name]
+        style_reference_features = layer_features_style[3, :, :, :]
+        for i in range(np.shape(K.eval(combination_img))[0]):
+            sl = style_loss(style_reference_features,
+                            layer_features_combination[i, :, :, :])
+            loss[i] += (0.1 / len(feature_layers)) * sl
+    loss += 0.05 * \
+        total_variation_loss(combination_img) * np.ones(np.shape(loss))
+    return K.variable(loss)
 
 
-pre_model = vgg16.VGG16(input_tensor=input_tensor,
-                    weights='imagenet', include_top=False)
+content_path = r'C:\Users\Renjiu Hu\Pictures\training data\content\\'
+files = utils.list_files(content_path)
+x_label = [utils.get_img(content_path + img).astype('float64')
+           for img in files]
+content_list = [K.variable(vgg16.preprocess_input(np.expand_dims(
+    img, axis=0))) for img in x_label]
+content_img = K.concatenate(content_list, axis=0)
 
-
-
-
-
+y_label = [np.random.uniform(0, 255, np.shape(x_label[0]))
+           for i in range(len(x_label))]
+#combination_img = K.placeholder((1, 256, 256, 3))
+combination_img = K.concatenate([K.variable(vgg16.preprocess_input(
+    np.expand_dims(img, axis=0))) for img in y_label], axis=0)
 
 model = Sequential()
 model.add(Conv2D(32, (9, 9), padding='same', activation='relu',
-                 input_shape=(None, 256, 256, None)))
-model.add(Conv2D(64, (3, 3), stride=2, padding='same', activation='relu'))
-model.add(Conv2D(128, (3, 3), stride=2, padding='same', activation='relu'))
-model.add(_residual_block((None, 256, 256, None)))
-model.add(_residual_block((None, 256, 256, None)))
-model.add(_residual_block((None, 256, 256, None)))
-model.add(_residual_block((None, 256, 256, None)))
-model.add(_residual_block((None, 256, 256, None)))
-model.add(_upsampling_block((None, 256, 256, None), 64))
-model.add(_upsampling_block((None, 256, 256, None), 32))
+                 input_shape=(256, 256, 3)))
+model.add(Conv2D(64, (3, 3), strides=2, padding='same', activation='relu'))
+model.add(Conv2D(128, (3, 3), strides=2, padding='same',
+                 activation='relu', name='conv3'))
+model.add(_residual_block((256, 256, 128)))
+model.add(_residual_block((256, 256, 128)))
+model.add(_residual_block((256, 256, 128)))
+model.add(_residual_block((256, 256, 128)))
+model.add(_residual_block((256, 256, 128)))
+model.add(_upsampling_block((256, 256, 128), 64))
+model.add(_upsampling_block((256, 256, 64), 32))
 model.add(Conv2D(3, (9, 9), padding='same', activation='sigmoid'))
 
 
-model.compile(optimizer='adam', loss='categorical_crossentropy',
-              metrics=['accuracy'])
-model.fit()
+for i in range(10):
+    model.compile(optimizer='adam', loss='loss',
+                  metrics=['accuracy'])
+    model.fit(content_img, combination_img, epoch=2, batch=2)
+    print("2 epochs finish")
+    combination_img = model.predict(combination_img, batch_size=2)
+    print("next turn starts")
+
+model.save('styleTrans.h5')
